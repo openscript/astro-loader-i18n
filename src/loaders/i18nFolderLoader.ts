0d73575d5fc259a306e8f04b43e813e2342d51b0
Robin <git+r@obin.ch>
2025-02-16 13:14:17 +0000

    chore: configure eslint

--- src/loaders/i18nFolderLoader.ts
+++ src/loaders/i18nFolderLoader.ts
@@ -3,6 +3,7 @@ import { Loader, LoaderContext } from "astro/loaders";
 export function i18nFolderLoader(): Loader {
   return {
     name: "i18n-folder-loader",
-    load: async (context: LoaderContext): Promise<void> => {}
+    load: async (context: LoaderContext): Promise<void> => {
+    }
   }
 }
