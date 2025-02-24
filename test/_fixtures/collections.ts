63256690bf8c1e43306e5ba9b5f807d8447195e4
Robin <git+r@obin.ch>
2025-02-24 08:06:50 +0000

    chore: add fixtures

---
+++ test/_fixtures/collections.ts
@@ -0,0 +1,111 @@
+import { DataEntry } from "astro:content";
+
+export const filesCollection: DataEntry[] = [
+  {
+    id: "about.de-CH",
+    filePath: "src/content/pages/about.de-CH.mdx",
+    data: {
+      title: "Über mich",
+    },
+    body: `---
+title: Über mich
+---
+
+Test
+`,
+  },
+  {
+    id: "projects.de-CH",
+    filePath: "src/content/pages/projects.de-CH.mdx",
+    data: {
+      title: "Projekte",
+    },
+    body: `---
+title: Projekte
+---
+
+Test
+`
+  },
+  {
+    id: "about.zh-CN",
+    filePath: "src/content/pages/about.zh-CN.mdx",
+    data: {
+      title: "关于我",
+    },
+    body: `---
+title: 关于我
+---
+
+测试
+`
+  },
+  {
+    id: "projects.zh-CN",
+    filePath: "src/content/pages/projects.zh-CN.mdx",
+    data: {
+      title: "项目",
+    },
+    body: `---
+title: 项目
+---
+
+测试
+`
+  },
+]
+
+export const folderCollection: DataEntry[] = [
+  {
+    id: "de-CH/about",
+    filePath: "src/content/pages/de-CH/about.mdx",
+    data: {
+      title: "Über mich",
+    },
+    body: `---
+title: Über mich
+---
+
+Test
+`,
+  },
+  {
+    id: "de-CH/projects",
+    filePath: "src/content/pages/de-CH/projects.mdx",
+    data: {
+      title: "Projekte",
+    },
+    body: `---
+title: Projekte
+---
+
+Test
+`
+  },
+  {
+    id: "zh-CN/about",
+    filePath: "src/content/pages/zh-CN/about.mdx",
+    data: {
+      title: "关于我",
+    },
+    body: `---
+title: 关于我
+---
+
+测试
+`
+  },
+  {
+    id: "zh-CN/projects",
+    filePath: "src/content/pages/zh-CN/projects.mdx",
+    data: {
+      title: "项目",
+    },
+    body: `---
+title: 项目
+---
+
+测试
+`
+  },
+];
