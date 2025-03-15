220d32ca3181f09d9bf3c31b8d5917745cf52634
Robin <git+r@obin.ch>
2025-03-15 07:37:09 +0000

    refactor: workspace layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "**/vite.config.*.timestamp*",
+      "**/vitest.config.*.timestamp*"
+    ]
+  }
 ];
