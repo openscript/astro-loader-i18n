2320adff1e100407e1706bedf1377b02afd5b897
Robin <git+r@obin.ch>
2025-03-20 06:02:25 +0000

    feat: path utility

--- libs/astro-loader-i18n/src/utils/path.ts
+++ libs/astro-loader-i18n/src/utils/path.ts
@@ -1,29 +1,29 @@
-const FILENAME_SUFFIX_PATTERN = /^([^.]+)\.?(.*)?(?=\.\w+)/;
-const ALL_SLASHES_PATTERN = /\//g;
-const INDEX_LOCALE_PAGES_ID = "index";
+const TRIM_SLASHES_PATTERN = /^\/|\/$/g;
+const DOUBLE_POINTS_PATTERN = /\.\./g;
+const DOUBLE_SLASH_PATTERN = /\/\//g;
+const INDEX_COMMON_TRANSLATION_ID = "index";
 
 export const trimSlashes = (path: string) => {
-  return path === "/" ? path : path.replace(/^\/|\/$/g, "");
+  return path === "/" ? path : path.replace(TRIM_SLASHES_PATTERN, "");
 };
 
-export const parseFilenameSuffix = (name: string, defaultLocale: string) => {
-  const nameMatch = name.match(FILENAME_SUFFIX_PATTERN);
-  const filename = nameMatch && nameMatch[1] ? nameMatch[1] : name;
-  const estimatedLocale = nameMatch && nameMatch[2] ? nameMatch[2] : defaultLocale;
+export const parseLocale = (path: string, locales: string[], defaultLocale: string) => {
+  const locale = locales.find((locale) => path.indexOf(locale) !== -1);
 
-  return { filename, estimatedLocale };
+  if (locale) {
+    return locale;
+  }
+
+  return defaultLocale;
 };
 
-export const createCommonTranslationId = (path: string, prefix?: string) => {
-  let commonTranslationId = trimSlashes(path).replace(ALL_SLASHES_PATTERN, ".");
+export const createCommonTranslationId = (path: string, pathLocale: string) => {
+  path = path.replace(pathLocale, "");
+  path = path.replace(DOUBLE_POINTS_PATTERN, "");
+  path = path.replace(DOUBLE_SLASH_PATTERN, "");
+  path = trimSlashes(path);
 
-  if (prefix) {
-    commonTranslationId = commonTranslationId.replace(new RegExp(`^${prefix}\\.`), "");
-  }
+  if (path === "") return INDEX_COMMON_TRANSLATION_ID;
 
-  if (commonTranslationId === "." || commonTranslationId === prefix) {
-    return INDEX_LOCALE_PAGES_ID;
-  }
-
-  return commonTranslationId;
+  return path;
 };
--- libs/astro-loader-i18n/test/utils/path.spec.ts
+++ libs/astro-loader-i18n/test/utils/path.spec.ts
@@ -1,70 +1,72 @@
 import { describe, expect, it } from "vitest";
-import { createCommonTranslationId, parseFilenameSuffix, trimSlashes } from "../../src/utils/path";
+import { createCommonTranslationId, parseLocale, trimSlashes } from "../../src/utils/path";
 
 describe("trimSlashes", () => {
-  it("should trim the slashes", () => {
+  it("both sides", () => {
     const trimmed = trimSlashes("/de/page/");
     expect(trimmed).toBe("de/page");
   });
 
-  it("shouldn't trim single slash", () => {
+  it("only slash", () => {
     const trimmed = trimSlashes("/");
     expect(trimmed).toBe("/");
   });
 
-  it("should trim only leading slash if there is no trailing slash", () => {
+  it("left side", () => {
     const trimmed = trimSlashes("/de/page");
     expect(trimmed).toBe("de/page");
   });
 
-  it("should trim only trailing slash if there is no leading slash", () => {
+  it("right side", () => {
     const trimmed = trimSlashes("de/page/");
     expect(trimmed).toBe("de/page");
   });
 });
 
-describe("parseFilenameSuffix", () => {
-  it("should estimate the language in a filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.de.md", "en");
-    expect(estimatedLocale).toBe("de");
+describe("parseLocale", () => {
+  it("locale from folder", () => {
+    const locale = parseLocale("/de-CH/page", ["de-CH", "zh-CN"], "zh-CN");
+    expect(locale).toBe("de-CH");
   });
 
-  it("should estimate the exact locale in a filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.de-CH.md", "en");
-    expect(estimatedLocale).toBe("de-CH");
+  it("locale from file suffix", () => {
+    const locale = parseLocale("/some/path/page.de-CH.md", ["de-CH", "zh-CN"], "zh-CN");
+    expect(locale).toBe("de-CH");
   });
 
-  it("should return the default locale if no locale was found in filename", () => {
-    const { estimatedLocale } = parseFilenameSuffix("index.md", "en");
-    expect(estimatedLocale).toBe("en");
-  });
-
-  it("should return the filename without extensions, even when there is no locale", () => {
-    const { filename } = parseFilenameSuffix("index.md", "en");
-    expect(filename).toBe("index");
-  });
-
-  it("should return the filename without extensions", () => {
-    const { filename } = parseFilenameSuffix("index.en-US.md", "en");
-    expect(filename).toBe("index");
+  it("default locale", () => {
+    const locale = parseLocale("/page", ["de-CH", "zh-CN"], "zh-CN");
+    expect(locale).toBe("zh-CN");
   });
 });
 
 describe("createCommonTranslationId", () => {
-  it("should return the locale page id", () => {
-    const commonTranslationId = createCommonTranslationId("/this/is/an/english-page");
-    expect(commonTranslationId).toBe("this.is.an.english-page");
+  it("no locale", () => {
+    const id = createCommonTranslationId("/page/", "zh-CN");
+    expect(id).toBe("page");
   });
-  it("should return the locale page id without prefix", () => {
-    const commonTranslationId = createCommonTranslationId("/en/this/is/an/english-page", "en");
-    expect(commonTranslationId).toBe("this.is.an.english-page");
+  it("index without locale", () => {
+    const id = createCommonTranslationId("/", "zh-CN");
+    expect(id).toBe("index");
   });
-  it("should return index for index pages", () => {
-    const commonTranslationId = createCommonTranslationId("/");
-    expect(commonTranslationId).toBe("index");
+  it("index with locale", () => {
+    const id = createCommonTranslationId("/zh-CN/", "zh-CN");
+    expect(id).toBe("index");
   });
-  it("should return index for index pages without prefix", () => {
-    const commonTranslationId = createCommonTranslationId("/en/", "en");
-    expect(commonTranslationId).toBe("index");
+  it("locale as prefix", () => {
+    const id = createCommonTranslationId("/zh-CN/page", "zh-CN");
+    expect(id).toBe("page");
+  });
+  it("locale as suffix", () => {
+    const id = createCommonTranslationId("/page/file.zh-CN.md", "zh-CN");
+    expect(id).toBe("page/filemd");
+  });
+  it("locale as suffix and trailing slash", () => {
+    const id = createCommonTranslationId("/page/file.zh-CN.md/", "zh-CN");
+    expect(id).toBe("page/filemd");
+  });
+  it("locale in between", () => {
+    const id = createCommonTranslationId("/page/zh-CN/file", "zh-CN");
+    expect(id).toBe("page/file");
   });
 });
