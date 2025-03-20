1e045d7c8c96ac40f63a21448afac425c107f4a7
Robin <git+r@obin.ch>
2025-03-20 07:05:19 +0000

    chore: add locale to entry data

--- libs/astro-loader-i18n/src/loaders/i18n-folder-loader.ts
+++ libs/astro-loader-i18n/src/loaders/i18n-folder-loader.ts
@@ -1,5 +1,5 @@
 import { glob, Loader, LoaderContext } from "astro/loaders";
-import { createCommonTranslationId } from "../utils/path";
+import { createCommonTranslationId, parseLocale } from "../utils/path";
 
 type GlobOptions = Parameters<typeof glob>[0];
 
@@ -11,14 +11,22 @@ export function i18nFolderLoader(options: GlobOptions): Loader {
       await globLoader.load(context);
       const entries = context.store.values();
 
+      if (!context.config.i18n) throw new Error("i18n configuration is missing in your astro config");
+
+      const { locales, defaultLocale } = context.config.i18n;
+      const localeCodes = locales.flatMap((locale) => (typeof locale === "string" ? locale : locale.codes));
+
       context.store.clear();
       entries.forEach((entry) => {
         if (!entry.filePath) return;
+        const locale = parseLocale(entry.filePath, localeCodes, defaultLocale);
+
         const newEntry = {
           ...entry,
           data: {
             ...entry.data,
-            commonTranslationId: createCommonTranslationId(entry.filePath),
+            locale,
+            commonTranslationId: createCommonTranslationId(entry.filePath, locale),
           },
         };
         context.store.set(newEntry);
