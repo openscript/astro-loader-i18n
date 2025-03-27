b2c73ae206eed667ab478c2a6bf46b05225517af
Robin <git+r@obin.ch>
2025-03-27 08:07:45 +0000

    chore: enhance example

--- apps/example/src/content.config.ts
+++ apps/example/src/content.config.ts
@@ -1,15 +1,27 @@
 import { defineCollection, z } from "astro:content";
-import { i18nLoader } from "astro-loader-i18n";
+import { i18nInfileSchema, i18nLoader } from "astro-loader-i18n";
+import { glob } from "astro/loaders";
+import astroConfig from "../astro.config";
 
-// const filesCollection = defineCollection("files", {});
+const filesCollection = defineCollection({
+  loader: i18nLoader({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/files", generateId: ({ entry }) => entry }),
+  schema: z.object({
+    title: z.string(),
+  }),
+});
 const folderCollection = defineCollection({
   loader: i18nLoader({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/folder", generateId: ({ entry }) => entry }),
   schema: z.object({
     title: z.string(),
   }),
 });
+const infileCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{yml,yaml}", base: "./src/content/infile", generateId: ({ entry }) => entry }),
+  schema: i18nInfileSchema(z.array(z.object({ path: z.string(), title: z.string() })), astroConfig.i18n!.locales),
+});
 
 export const collections = {
-  //  files: filesCollection,
+  files: filesCollection,
   folder: folderCollection,
+  infile: infileCollection,
 };
