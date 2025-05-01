9c8434f491105768334432bd8d873ed43848eb4c
Robin <git+r@obin.ch>
2025-05-01 15:05:47 +0000

    chore: clean up project

--- libs/astro-loader-i18n/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type NormalizeLocales<T extends Locales> = {
-  [K in keyof T]: T[K] extends string
-    ? T[K]
-    : T[K] extends {
-          codes: Array<string>;
-        }
-      ? T[K]["codes"][number]
-      : never;
-}[number];
